495d9da4ae12be449449590383f0f2b8dbc2151a
StephenDsouza90 <dsouza_stephen@hotmail.com>
2019-10-28 21:32:21 +0100

    Broke main into smaller parts

--- FoodOrderingSystem.py
+++ FoodOrderingSystem.py
@@ -282,11 +282,11 @@ class Customer:
 
         try:
             login = session.query(CustomerDetails).filter_by(cust_id=cust_id).first()
+            return login
         except Exception as ex:
             print("Error getting customer ID, error={}".format(str(ex)))
         finally:
-            session.close()
-        return login    
+            session.close()            
          
     def create_order_id(self, cust_id, session): 
         """ Generate order id """
@@ -543,12 +543,8 @@ class Controller(SQLiteBackend):
 
     def customer_login(self, cust_id):
         session = self.Session()
-        c = self.customer.customer_login(cust_id, session)
-        if c:
-            print("Login successful! \nWelcome {} \nCustomer ID {}".format(c.cust_name, c.cust_id))
-        else:
-            print("Login not sucessfully. Please try again or signup!")
-            return
+        login = self.customer.customer_login(cust_id, session)
+        return login
 
     def process_order(self, cust_id):
         session = self.Session()
@@ -593,7 +589,7 @@ class Controller(SQLiteBackend):
                 else:
                     print("Items not updated")
 
-            option = int(input("\nSelect option: "))
+            option = int(input(selection))
         
         if o:
             print("\nYour order is generated. \nOrder number: {}".format(o.order_id))
@@ -671,6 +667,200 @@ class DeliveryPerson(Base, Employee):
             session.expunge_all()
             session.close()
 
+def process_employee_options_flow(fos):
+    emp_options = """
+    Welcome to the employee interface! Please press the below options:
+        
+    0. Quit
+    1. Add food category
+    2. Add food details
+    3. Add delivery person
+    4. Assign delivery person to order 
+    5. Update order (Only for delivery person use)
+    6. View order details
+    7. View order status
+    8. View revenue today
+    9. Delete order
+    
+    Your Option:
+
+    """
+
+    employee_options = int(input(emp_options))
+
+    while employee_options != 0:
+
+        if employee_options == 1:
+            category_name = input("Enter category name: ")
+            fos.add_food_category(category_name)
+
+        elif employee_options == 2:
+            category_id = input ("Enter category ID: ")
+            food_name = input("Enter food name: ")
+            price = input("Food price: ")
+            fos.add_food_details(category_id, food_name, price)
+
+        elif employee_options == 3:
+            delivery_person_name = input("Enter delivery person name: ")
+            delivery_person_phone = input("Enter delivery person phone: ")
+            fos.add_delivery_person(delivery_person_name, delivery_person_phone)
+
+        elif employee_options == 4:
+            order_id = input("Enter order ID: ")
+            delivery_person_id = input("Enter delivery person ID: ")
+            fos.assign_deliver_person_to_deliver_order(order_id, delivery_person_id) 
+
+        elif employee_options == 5:
+            order_id = input("Enter order ID: ")
+            select = """                 
+            To update order, press
+                
+            1. En route
+            2. Delivered
+
+            Your option: 
+ 
+            """
+                
+            options = int(input(select))
+            if options == 1:
+                order_status = "En route"
+                fos.update_order(order_id, order_status) 
+
+            if options == 2:
+                order_status = "Delivered"
+                fos.update_order(order_id, order_status)
+
+        elif employee_options == 6:
+            order_id = input("Enter order ID: ")
+            fos.view_orders(order_id)
+            fos.view_order_grand_total(order_id)
+
+        elif employee_options == 7:
+            order_id = input("Enter order ID: ")
+            fos.view_order_status(order_id)
+            fos.view_orders(order_id)
+
+        elif employee_options == 8:
+            select = """ 
+            To see revenue for a certain status, please press 
+                
+            1. Checkedout
+            2. En route
+            3. Delivered
+                
+            Your option:                 
+
+            """                
+            options = int(input(select))
+            if options == 1:
+                order_status = "Checkedout"
+            elif options == 2:
+                order_status = "En route"
+            else:
+                order_status = "Delivered"
+            fos.view_sales_today(order_status)
+            fos.sum_revenue_today(order_status)
+                
+        elif employee_options == 9:
+            order_id = input("Enter order ID: ")
+            fos.delete_order(order_id)
+
+        employee_options = int(input(emp_options))
+
+def process_order_flow(fos):
+    selection = """ 
+
+    0. Logout
+    1. Process order
+    2. View order
+    3. Checkout
+    4. Cancel order
+    5. View order status
+                
+    Select option: 
+
+    """
+    order = int(input(selection))
+
+    while order != 0:
+
+        if order == 1:
+            cust_id = input("Enter customer ID: ")
+            fos.process_order(cust_id)
+
+        elif order == 2:
+            order_id = input("Enter order ID: ")
+            fos.view_order(order_id)
+                
+        elif order == 3:
+            order_id = input("Enter order ID: ")
+            order_address = input("Enter delivery address: ")
+            checkout = int(input("Press 1 to confirm checkout: "))
+            if checkout == 1:
+                order_status = "Checkedout"
+            checkout_time = datetime.now()
+            delivery_time = timedelta(minutes=30)
+            estimated_time = checkout_time + delivery_time
+            session = fos.Session()
+            view_grand_total = fos.customer.view_order_grand_total(order_id, session)
+            if view_grand_total:
+                for fd, cos, cd, cosa, t in view_grand_total:
+                    bill_amount = t
+            fos.checkout(order_id, order_status, order_address, checkout_time, estimated_time, bill_amount)
+
+        elif order == 4:
+            order_id = input("Enter order ID: ")
+            cancel = int(input("Press 1 to confirm cancellation: "))
+            if cancel == 1:
+                order_status = "Cancelled"
+            fos.cancel_order(order_id, order_status) 
+
+        elif order == 5:
+            order_id = input("Enter order ID: ")
+            fos.view_orders_status(order_id)
+
+        order = int(input(selection))               
+
+def process_customer_options_flow(fos):
+    cust_options = """ 
+        
+    Welcome to the employee interface! Please press the below options:
+        
+    0. Quit
+    1. View menu
+    2. Customer signup
+    3. Customer login
+
+    Your Option:
+
+    """        
+        
+    customer_options = int(input(cust_options))
+
+    while customer_options != 0:
+        
+        if customer_options == 1:
+            fos.view_menu()
+
+        elif customer_options == 2:
+            cust_name = input("Enter customer name: ")
+            cust_phone = input("Enter customer phone number: ")
+            cust_email = input("Enter customer email address: ")
+            fos.customer_signup(cust_name, cust_phone, cust_email)
+
+        elif customer_options == 3:
+            cust_id = input("Enter customer ID: ")
+            c = fos.customer_login(cust_id)
+            if c:
+                print("Login successful! \nWelcome {} \nCustomer ID {}".format(c.cust_name, c.cust_id))
+            else:
+                print("Login not sucessfully. Please try again or signup!")
+                break
+            process_order_flow(fos)
+
+        customer_options = int(input(cust_options))
+
 def main():
     """ The user interface in which employee/customer will use to perform actions on the Food Ordering System. """
 
@@ -693,192 +883,9 @@ def main():
     option = int(input(welcome_message))
 
     if option == 1:
-        emp_options = """         
-        Welcome to the employee interface! Please press the below options:
-        
-        0. Quit
-        1. Add food category
-        2. Add food details
-        3. Add delivery person
-        4. Assign delivery person to order 
-        5. Update order (Only for delivery person use)
-        6. View order details
-        7. View order status
-        8. View revenue today
-        9. Delete order
+        process_employee_options_flow(fos)
     
-        Your Option:
-
-        """
-
-        employee_options = int(input(emp_options))
-
-        while employee_options != 0:
-
-            if employee_options == 1:
-                category_name = input("Enter category name: ")
-                fos.add_food_category(category_name)
-
-            elif employee_options == 2:
-                category_id = input ("Enter category ID: ")
-                food_name = input("Enter food name: ")
-                price = input("Food price: ")
-                fos.add_food_details(category_id, food_name, price)
-
-            elif employee_options == 3:
-                delivery_person_name = input("Enter delivery person name: ")
-                delivery_person_phone = input("Enter delivery person phone: ")
-                fos.add_delivery_person(delivery_person_name, delivery_person_phone)
-
-            elif employee_options == 4:
-                order_id = input("Enter order ID: ")
-                delivery_person_id = input("Enter delivery person ID: ")
-                fos.assign_deliver_person_to_deliver_order(order_id, delivery_person_id) 
-
-            elif employee_options == 5:
-                order_id = input("Enter order ID: ")
-                select = """                 
-                To update order, press
-                
-                1. En route
-                2. Delivered
-
-                Your option: 
- 
-                """
-                
-                options = int(input(select))
-                if options == 1:
-                    order_status = "En route"
-                    fos.update_order(order_id, order_status) 
-
-                if options == 2:
-                    order_status = "Delivered"
-                    fos.update_order(order_id, order_status)
-
-            elif employee_options == 6:
-                order_id = input("Enter order ID: ")
-                fos.view_orders(order_id)
-                fos.view_order_grand_total(order_id)
-
-            elif employee_options == 7:
-                order_id = input("Enter order ID: ")
-                fos.view_order_status(order_id)
-                fos.view_orders(order_id)
-
-            elif employee_options == 8:
-                select = """ 
-                To see revenue for a certain status, please press 
-                
-                1. Checkedout
-                2. En route
-                3. Delivered
-                
-                Your option:                 
-
-                """                
-                options = int(input(select))
-                if options == 1:
-                    order_status = "Checkedout"
-                elif options == 2:
-                    order_status = "En route"
-                else:
-                    order_status = "Delivered"
-                fos.view_sales_today(order_status)
-                fos.sum_revenue_today(order_status)
-                
-            elif employee_options == 9:
-                order_id = input("Enter order ID: ")
-                fos.delete_order(order_id)
-
-            employee_options = int(input("\nYour option: "))
-            
     elif option == 2:
-        cust_options = """ 
-        
-        Welcome to the employee interface! Please press the below options:
-        
-        0. Quit
-        1. View menu
-        2. Customer signup
-        3. Customer login
-
-        Your Option:
-
-        """        
-        
-        customer_options = int(input(cust_options))
-
-        while customer_options != 0:
-
-            if customer_options == 1:
-                fos.view_menu()
-
-            elif customer_options == 2:
-                cust_name = input("Enter customer name: ")
-                cust_phone = input("Enter customer phone number: ")
-                cust_email = input("Enter customer email address: ")
-                fos.customer_signup(cust_name, cust_phone, cust_email)
-
-            elif customer_options == 3:
-                cust_id = input("Enter customer ID: ")
-                fos.customer_login(cust_id)
-
-                selection = """ 
-
-                0. Logout
-                1. Process order
-                2. View order
-                3. Checkout
-                4. Cancel order
-                5. View order status
-                
-                Select option: 
-
-                """
-                order = int(input(selection))
-
-                while order != 0:
-
-                    if order == 1:
-                        fos.process_order(cust_id)
-
-                    elif order == 2:
-                        order_id = input("Enter order ID: ")
-                        fos.view_order(order_id)
-                    # Problem - A customer can look at any order using the order ID. Need to relook how to solve this.
-                
-                    elif order == 3:
-                        order_id = input("Enter order ID: ")
-                        order_address = input("Enter delivery address: ")
-                        checkout = int(input("Press 1 to confirm checkout: "))
-                        if checkout == 1:
-                            order_status = "Checkedout"
-                        checkout_time = datetime.now()
-                        delivery_time = timedelta(minutes=30)
-                        estimated_time = checkout_time + delivery_time
-                        session = fos.Session()
-                        view_grand_total = fos.customer.view_order_grand_total(order_id, session)
-                        if view_grand_total:
-                            for fd, cos, cd, cosa, t in view_grand_total:
-                                bill_amount = t
-                        fos.checkout(order_id, order_status, order_address, checkout_time, estimated_time, bill_amount)
-                    # Problem - A customer can look at any order using the order ID. Need to relook how to solve this.
-
-                    elif order == 4:
-                        order_id = input("Enter order ID: ")
-                        cancel = int(input("Press 1 to confirm cancellation: "))
-                        if cancel == 1:
-                            order_status = "Cancelled"
-                        fos.cancel_order(order_id, order_status) 
-                    # Problem - A customer can look at any order using the order ID. Need to relook how to solve this.
-
-                    elif order == 5:
-                        order_id = input("Enter order ID: ")
-                        fos.view_orders_status(order_id)
-
-                    order = int(input("\nSelect option: "))               
-
-            customer_options = int(input("\nYour option: "))
+        process_customer_options_flow(fos)
 
 main()
