c1b58424eb4d97a9de54509f475a596e40ac5795
StephenDsouza90 <dsouza_stephen@hotmail.com>
2019-10-30 15:47:41 +0100

    Split logic into multiple files

---
+++ core.py
@@ -0,0 +1,205 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.exc import IntegrityError
+
+import constants
+from models import Employee, Customer, DeliveryPerson, SQLiteBackend
+
+
+class Controller(SQLiteBackend):
+    """ Controller class that inherites from SQLite Backend and composition 
+        from Emploeyee, Customer and Delivery Person classes """
+
+    def __init__(self, db_url):
+        super(Controller, self).__init__(db_url)
+        self.employee = Employee()
+        self.customer = Customer()
+        self.delivery_person = DeliveryPerson()
+
+
+    def add_food_category(self, category_name):
+        session = self.Session()
+        self.employee.add_food_category(category_name, session)
+        print("\nAdd {}".format("Successfully" if True else "not successfully"))
+
+
+    def add_food_details(self, category_id, food_name, price):
+        session = self.Session()
+        self.employee.add_food_details(category_id, food_name, price, session)
+        print("\nAdd {}".format("Successfully" if True else "not successfully"))
+
+
+    def add_delivery_person(self, delivery_person_name, delivery_person_phone):
+        session = self.Session()
+        d = self.employee.add_delivery_person(delivery_person_name, delivery_person_phone, session)
+        if d:
+            print("\nAdd {} \nDeliver person ID: {} ".format(
+                "Successfully", d.delivery_person_id if True else  "not successfully"))
+
+
+    def assign_deliver_person_to_deliver_order(self, order_id, delivery_person_id):
+        session = self.Session()
+        self.employee.assign_deliver_person_to_deliver_order(order_id, delivery_person_id, session) 
+        print("Add {}".format("sucessful" if True else "not sucessful")) 
+
+
+    def update_order(self, order_id, order_status):
+        session = self.Session()
+        self.delivery_person.update_order(order_id, order_status, session) 
+        print("Order update {}".format("successful" if True else "not successful"))
+
+
+    def view_orders(self, order_id):
+        session = self.Session()
+        view_order = self.employee.view_orders(order_id, session)
+        if view_order:
+            for fc, fd, cos in view_order:
+                print("\nFood category: {} \nFood name: {} \nFood price: {} \nFood quantity: {} \nPrice per item: {}".format(
+                    fc.name, fd.food_name, fd.price, cos.food_qty, (fd.price*cos.food_qty)))
+                
+
+    def view_order_grand_total(self, order_id):
+        session = self.Session()
+        view_order_grand = self.employee.view_order_grand_total(order_id, session)
+        if view_order_grand:
+            for cd, cosa in view_order_grand:                        
+                print("\nCustomer name: {} \nOrder ID: {} \nTotal bill: {}".format(
+                    cd.cust_name, cosa.order_id, cosa.bill_amount))                
+
+
+    def view_order_status(self, order_id):
+        session = self.Session()
+        view_status = self.employee.view_order_status(order_id, session)
+        if view_status:
+            for cd, cosa, dp in view_status:
+                print("\nCustomer name: {} \nOrder ID: {} \nDeliver person name: {} \nOrder status: {} \nTotal bill: {}".format(
+                    cd.cust_name, cosa.order_id, dp.delivery_person_name, cosa.order_status, cosa.bill_amount))
+
+
+    def view_sales_today(self, order_status):
+        session = self.Session()
+        sales_today = self.employee.view_sales_today(order_status, session)
+        if sales_today:
+            for cos, cd in sales_today:
+                print("\nCustomer name: {} \nOrder ID: {} \nOrder Status: {} \nBill amount: {} \nDate & Time: {}".format(
+                    cd.cust_name, cos.order_id, cos.order_status, cos.bill_amount, cos.checkout_time))
+
+
+    def sum_revenue_today(self, order_status):
+        session = self.Session()
+        sum_rev_today = self.employee.sum_revenue_today(order_status, session)
+        if sum_rev_today:
+            for cos, s in sum_rev_today:
+                print("\nToday's revenue: {} ".format(s))
+
+
+    def delete_order(self, order_id):
+        session = self.Session()
+        self.employee.delete_order(order_id, session)
+        print("\nOrder {} deleted!".format(order_id))
+
+
+    def view_menu(self):
+        session = self.Session()
+        menu = self.customer.view_menu(session)
+        for fc, fd in menu:
+            print("\nFood ID: {} \nFood category: {} \nFood name: {} \nFood price: {}".format(
+                fd.food_id, fc.name, fd.food_name, fd.price))
+
+
+    def customer_signup(self, cust_name, cust_phone, cust_email):
+        session = self.Session()
+        c = self.customer.customer_signup(cust_name, cust_phone, cust_email, session)
+        if c:
+            print("\nSignup {}! Customer ID: {}".format("successfully", c.cust_id if True else "not successfully"))
+
+
+    def customer_login(self, cust_id):
+        session = self.Session()
+        login = self.customer.customer_login(cust_id, session)
+        return login
+
+
+    def process_order(self, cust_id):
+        session = self.Session()
+        o = self.customer.create_order_id(cust_id, session)
+        selection = """        
+        0. Back
+        1. Add food to order
+        2. Remove food to order
+        3. Update food to order
+                
+        Select option: 
+
+        """
+        option = int(input(selection))
+
+        while option != constants.CUST_OPT_BACK:
+            
+            if option == constants.CUST_OPT_ADD_FOOD_TO_ORDER:
+                food_id = input("Enter food ID: ")
+                food_qty = input("Enter food quantity: ")
+                self.customer.add_food_to_order(o.order_id, food_id, food_qty, session)
+                print("Add {}!".format("successful" if True else "not successful."))
+
+            elif option == constants.CUST_OPT_REMOVE_FOOD_TO_ORDER:
+                food_id = input("Enter food ID: ")
+                self.customer.remove_food_to_order(o.order_id, food_id, session)
+                print("Items {}".format("removed" if True else "not removed"))
+
+            elif option == constants.CUST_OPT_UPDATE_FOOD_TO_ORDER:
+                food_id = input("Enter food ID: ")
+                food_qty = input("Enter food quantity: ")
+                self.customer.update_food_to_order(o.order_id, food_id, food_qty, session)
+                print("Items {}".format("updated" if True else "not updated"))
+
+            option = int(input(selection))
+        
+        if o:
+            print("Order {}. \nOrder number: {}".format("generated", o.order_id if True else "not generated"))
+
+
+    def view_order(self, order_id):
+        session = self.Session()
+        view_order_item = self.customer.view_order_per_item(order_id, session)
+        if view_order_item:
+            for fc, fd, cos in view_order_item:
+                print("\nFood category: {} \nFood name: {} \nFood price: {} \nFood quantity: {} \nTotal per item: {}".format(
+                    fc.name, fd.food_name, fd.price, cos.food_qty, (fd.price*cos.food_qty)))
+
+        view_order_grand = self.customer.view_order_grand_total(order_id, session)
+        if view_order_grand:
+            for fd, cos, cd, cosa, t in view_order_grand:
+                print("\nCustomer name: {} \nOrder ID: {} \nTotal bill: {}".format(cd.cust_name, cosa.order_id, t))
+
+
+    def checkout(self, order_id, order_status, order_address, checkout_time, estimated_time, bill_amount):
+        session = self.Session()
+        c = self.customer.checkout(order_id, order_status, order_address, 
+                        checkout_time, estimated_time, bill_amount, session) 
+        if c:
+            print("Checkout {}!".format("successful" if True else "not successful"))
+
+
+    def cancel_order(self, order_id, order_status):
+        session = self.Session()
+        c = self.customer.cancel_order(order_id, order_status, session) 
+        if c:
+            print("Cancel {}!".format("successful" if True else "not successful"))
+        
+
+    def view_orders_status(self, order_id):
+        session = self.Session()
+        view_order_item = self.customer.view_order_per_item(order_id, session)
+        if view_order_item:
+            for fc, fd, cos in view_order_item:
+                print("\nFood category: {} \nFood name: {} \nFood price: {} \nFood quantity: {} \nTotal per item: {}".format(
+                    fc.name, fd.food_name, fd.price, cos.food_qty, (fd.price*cos.food_qty)))
+                        
+        view_status = self.customer.view_orders_status(order_id, session)
+        if view_status:
+            for cos, cd, cosa, dp in view_status:                        
+                print("\nCustomer name: {} \nOrder ID: {} \nDeliver person name: {} \nOrder status: {} \nTotal price: {}".format(
+                    cd.cust_name, cosa.order_id, dp.delivery_person_name, cosa.order_status, cosa.bill_amount))
+    # Printing it 3 times - need to review.
---
+++ main.py
@@ -0,0 +1,230 @@
+import time
+from datetime import datetime, timedelta
+
+import constants
+from core import Controller
+
+
+def process_employee_options_flow(fos):
+    emp_options = """
+    Welcome to the employee interface! Please press the below options:
+        
+    0. Quit
+    1. Add food category
+    2. Add food details
+    3. Add delivery person
+    4. Assign delivery person to order 
+    5. Update order (Only for delivery person use)
+    6. View order details
+    7. View order status
+    8. View revenue today
+    9. Delete order
+    
+    Your Option:
+
+    """
+
+    employee_options = int(input(emp_options))
+
+    while employee_options != constants.EMP_OPT_QUIT:
+
+        if employee_options == constants.EMP_OPT_ADD_FOOD_CATEGORY:
+            category_name = input("Enter category name: ")
+            fos.add_food_category(category_name)
+
+        elif employee_options == constants.EMP_OPT_ADD_FOOD_DETAILS:
+            category_id = input ("Enter category ID: ")
+            food_name = input("Enter food name: ")
+            price = input("Food price: ")
+            fos.add_food_details(category_id, food_name, price)
+
+        elif employee_options == constants.EMP_OPT_ADD_DELIVERY_PERSON:
+            delivery_person_name = input("Enter delivery person name: ")
+            delivery_person_phone = input("Enter delivery person phone: ")
+            fos.add_delivery_person(delivery_person_name, delivery_person_phone)
+
+        elif employee_options == constants.EMP_OPT_ASSIGN_DELIVERY_PERSON_TO_ORDER:
+            order_id = input("Enter order ID: ")
+            delivery_person_id = input("Enter delivery person ID: ")
+            fos.assign_deliver_person_to_deliver_order(order_id, delivery_person_id) 
+
+        elif employee_options == constants.EMP_OPT_UPDATE_ORDER:
+            order_id = input("Enter order ID: ")
+            select = """                 
+            To update order, press
+                
+            1. En route
+            2. Delivered
+
+            Your option: 
+ 
+            """
+                
+            options = int(input(select))
+            if options == constants.EMP_OPT_UO_EN_ROUTE:
+                order_status = "En route"
+                fos.update_order(order_id, order_status) 
+
+            elif options == constants.EMP_OPT_UO_DELIVERED:
+                order_status = "Delivered"
+                fos.update_order(order_id, order_status)
+
+        elif employee_options == constants.EMP_OPT_VIEW_ORDER_DETAILS:
+            order_id = input("Enter order ID: ")
+            fos.view_orders(order_id)
+            fos.view_order_grand_total(order_id)
+
+        elif employee_options == constants.EMP_OPT_VIEW_ORDER_STATUS:
+            order_id = input("Enter order ID: ")
+            fos.view_order_status(order_id)
+            fos.view_orders(order_id)
+
+        elif employee_options == constants.EMP_OPT_VIEW_REVENUE_TODAY:
+            select = """ 
+            To see revenue for a certain status, please press 
+                
+            1. Checkedout
+            2. En route
+            3. Delivered
+                
+            Your option:                 
+
+            """                
+            options = int(input(select))
+            if options == constants.EMP_OPT_RT_CHECKEDOUT:
+                order_status = "Checkedout"
+            elif options == constants.EMP_OPT_RT_EN_ROUTE:
+                order_status = "En route"
+            else:
+                order_status = "Delivered"
+            fos.view_sales_today(order_status)
+            fos.sum_revenue_today(order_status)
+                
+        elif employee_options == constants.EMP_OPT_DELETE_ORDER:
+            order_id = input("Enter order ID: ")
+            fos.delete_order(order_id)
+
+        employee_options = int(input(emp_options))
+
+
+def process_order_flow(fos):
+    selection = """ 
+    0. Logout
+    1. Process order
+    2. View order
+    3. Checkout
+    4. Cancel order
+    5. View order status
+                
+    Select option: 
+
+    """
+    order = int(input(selection))
+
+    while order != constants.CUST_OPT_LOGOUT:
+
+        if order == constants.CUST_OPT_PROCESS_ORDER:
+            cust_id = input("Enter customer ID: ")
+            fos.process_order(cust_id)
+
+        elif order == constants.CUST_OPT_VIEW_ORDER:
+            order_id = input("Enter order ID: ")
+            fos.view_order(order_id)
+                
+        elif order == constants.CUST_OPT_CHECKOUT:
+            order_id = input("Enter order ID: ")
+            order_address = input("Enter delivery address: ")
+            checkout = int(input("Press 1 to confirm checkout: "))
+            if checkout == constants.CUST_OPT_CHECKEDOUT:
+                order_status = "Checkedout"
+            checkout_time = datetime.now()
+            delivery_time = timedelta(minutes=30)
+            estimated_time = checkout_time + delivery_time
+            session = fos.Session()
+            view_grand_total = fos.customer.view_order_grand_total(order_id, session)
+            if view_grand_total:
+                for fd, cos, cd, cosa, grand_total in view_grand_total:
+                    bill_amount = grand_total
+            fos.checkout(order_id, order_status, order_address, checkout_time, estimated_time, bill_amount)
+
+        elif order == constants.CUST_OPT_CANCEL_ORDER:
+            order_id = input("Enter order ID: ")
+            cancel = int(input("Press 1 to confirm cancellation: "))
+            if cancel == constants.CUST_OPT_CANCELLED:
+                order_status = "Cancelled"
+            fos.cancel_order(order_id, order_status) 
+
+        elif order == constants.CUST_OPT_VIEW_ORDER_STATUS:
+            order_id = input("Enter order ID: ")
+            fos.view_orders_status(order_id)
+
+        order = int(input(selection))               
+
+
+def process_customer_options_flow(fos):
+    cust_options = """ 
+    Welcome to the customer interface! Please press the below options:
+        
+    0. Quit
+    1. View menu
+    2. Customer signup
+    3. Customer login
+
+    Your Option:
+
+    """        
+        
+    customer_options = int(input(cust_options))
+
+    while customer_options != constants.CUST_OPT_QUIT:
+        
+        if customer_options == constants.CUST_OPT_VIEW_MENU:
+            fos.view_menu()
+
+        elif customer_options == constants.CUST_OPT_CUSTOMER_SIGNUP:
+            cust_name = input("Enter customer name: ")
+            cust_phone = input("Enter customer phone number: ")
+            cust_email = input("Enter customer email address: ")
+            fos.customer_signup(cust_name, cust_phone, cust_email)
+
+        elif customer_options == constants.CUST_OPT_CUSTOMER_LOGIN:
+            cust_id = input("Enter customer ID: ")
+            c = fos.customer_login(cust_id)
+            if c:
+                print("Login successful! \nWelcome {}".format(c.cust_name))
+                process_order_flow(fos)
+            else:
+                print("Login not successfully. Please try again or signup!")
+                            
+        customer_options = int(input(cust_options))
+
+
+def main():
+    """ The user interface in which employee/customer will use to 
+    perform actions on the Food Ordering System. """
+
+    db_url = "sqlite:///fos2.db"
+    print("Connecting to {}".format(db_url))
+
+    fos = Controller(db_url)
+    fos.bootstrap()
+
+    welcome_message = """ 
+    Welcome to the Food Ordering System! Please press the below options:
+
+    1. Employee
+    2. Customer
+
+    Your Option:
+
+    """
+
+    option = int(input(welcome_message))
+
+    if option == constants.EMPLOYEE:
+        process_employee_options_flow(fos)
+    
+    elif option == constants.CUSTOMER:
+        process_customer_options_flow(fos)
+
+main()
